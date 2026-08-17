0a62a56fe29d248d5b03fd58a0b3fa1b33f6f179
mohamed23232 <42505614+mohamed23232@users.noreply.github.com>
2026-08-17 13:44:07 +0300

    modify ui

---
+++ Assets/_Project/_Scripts/UI/FloatingStar.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FloatingStar : MonoBehaviour
+{
+    [SerializeField] private float horizontalAmount = 0.15f;
+    [SerializeField] private float verticalAmount = 0.2f;
+
+    [SerializeField] private float horizontalSpeed = 0.7f;
+    [SerializeField] private float verticalSpeed = 1f;
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float x = Mathf.Sin(Time.time * horizontalSpeed) * horizontalAmount;
+        float y = Mathf.Sin(Time.time * verticalSpeed) * verticalAmount;
+
+        transform.position = startPosition + new Vector3(x, y, 0f);
+    }
+}
